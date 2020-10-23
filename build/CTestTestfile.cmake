5b9243bc389547a666b9726573638c5a32c91f4b
iamdevgeek <iamdevgeek@gmail.com>
2020-10-23 09:50:39 -0700

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/swetang/new_catkin_ws/src
+# Build directory: /home/swetang/new_catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("roslab0")
---
+++ build/roslab0/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/swetang/new_catkin_ws/src/roslab0
+# Build directory: /home/swetang/new_catkin_ws/build/roslab0
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
